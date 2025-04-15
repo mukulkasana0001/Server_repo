8a6aee896d73c5f9172aacdb2aa9b5951544ed73
mukul kasana <mukulkasana0001@gmail.com>
2025-04-15 12:01:10 +0530

    new

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log("Server listening on https://server-repo-phi.vercel.app");
+  console.log("Server listening on https://mukulkasana0001.github.io/Server_repo/");
 });

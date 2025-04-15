066688f40b1c461ce0dc177a7bcb32611dd58fbd
mukul kasana <mukulkasana0001@gmail.com>
2025-04-15 11:52:09 +0530

    added new

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://vercel.com/mukulkasana0001s-projects/chat-app", 
+    origin:  [
+      // "http://localhost:3000",
+      "https://chat-app-mukulkasana0001s-projects.vercel.app"
+    ], 
   }
 });
 

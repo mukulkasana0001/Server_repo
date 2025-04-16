bc926dcdc12451b4ffdc445162f5eb13063c9891
mukul kasana <mukulkasana0001@gmail.com>
2025-04-16 12:10:47 +0530

    new

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
   cors: {
     origin:  [
       // "http://localhost:3000",
-      "https://chat-app-mukulkasana0001s-projects.vercel.app"
+      "https://chat-app-ecru-xi-77.vercel.app"
     ], 
   }
 });
